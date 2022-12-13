2246b4c3485a2bb9da8e2ee5742c9b6fecc06684
Monjul Boruah <65394514+monjulboruah@users.noreply.github.com>
2022-12-13 15:32:22 +0000

    initial setup

---
+++ backend/model/candidateSchema.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require("mongoose");
+
+const candidateSchema = new Schema(
+  {
+    nameOfCandidate: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    mobNo: String,
+    dob: String,
+    workExp: String,
+    resumeTitle: String,
+    currLoc: String,
+    postalAdd: String,
+    pointOfOccurance: String,
+    currEmployer: Boolean,
+    currDesgn: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Candidate", candidateSchema);
