4687f53cfbe248e241c2c2c4022ea8dd03a8199c
Monjul Boruah <65394514+monjulboruah@users.noreply.github.com>
2022-12-14 16:22:30 +0000

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 
 function App() {
   const [image, setImage] = useState({ preview: '', data: '' })
-  const [status, setStatus] = useState('')
+  const [status, setStatus] = useState(false)
+  const[loading, setLoading] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setLoading(true)
     let formData = new FormData()
     formData.append('file', image.data)
-    const response = await fetch('https://monjulboruah-scaling-zebra-j9rrrr7pww5249x-5000.preview.app.github.dev/create', {
+    const response = await fetch('http://localhost:5002/create', {
       method: 'POST',
       body: formData,
     })
-    if (response) setStatus(response.statusText)
+    if (response) {
+      setStatus(false)
+      alert("Success")
+    }
+    else setStatus(true)
+    setLoading(false)
   }
 
   const handleFileChange = (e) => {
@@ -24,14 +32,21 @@ function App() {
   }
   return (
     <div className='App'>
-      <h1>Upload to server</h1>
-      {image.preview && <img src={image.preview} width='100' height='100' />}
-      <hr></hr>
-      <form onSubmit={handleSubmit}>
-        <input type='file' name='file' onChange={handleFileChange}></input>
-        <button type='submit'>Submit</button>
-      </form>
-      {status && <h4>{status}</h4>}
+      {
+        loading === true ? <p>Loading... | Please Wait </p>
+        : (
+        <>
+          <h1>Upload to server</h1>
+         
+          <form onSubmit={handleSubmit}>
+            <input type='file' name='file' onChange={handleFileChange}></input>
+            <button type='submit'>Submit</button>
+          </form>
+        </>
+        )
+      }
+      {status === true ? <p>Upload failed</p> : <></>}
+      
     </div>
   )
 }
